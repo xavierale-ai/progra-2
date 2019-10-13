d37904fea6dc5c8455a1094686ca6d6fe79227bd
xavierale-ai <xavierale2000@gmail.com>
2019-10-13 15:58:41 -0600

    hi

---
+++ progra2/src/main/java/Datos/ColaPrioridad_1.java
@@ -0,0 +1,121 @@
+package Datos;
+
+/**
+ *
+ * @author julian
+ */
+public class ColaPrioridad_1<T>
+{
+    private class Node<T> {
+		
+		//atributos
+		private Object element;
+		private Node next;
+		
+		//Constructores
+		public Node() {
+			this.element = null;
+			this.next = null;
+		}
+		
+		public Node(Object element) {
+			this.element = element;
+			this.next = null;
+		}
+		
+		public Node(Object element, Node next) {
+			this.element = element;
+			this.next = next;
+		}
+		
+		//metodos
+		public Object getElement() {
+			return this.element;
+		}
+		
+		public void setElement(Object element) {
+			this.element = element;
+		}
+		
+		public Node getNext() {
+			return this.next;
+		}
+		
+		public void setNext(Node next) {
+			this.next = next;	
+		}
+	}
+	
+    
+	private Node<T> front;
+	private Node<T> rear;
+	private int size;
+	
+	public ColaPrioridad_1(){
+		this.front = new Node<>();
+		this.rear = this.front;
+		this.size = 0;
+	}
+	
+	public void enqueue(T element){
+		this.rear.setNext(new Node<T>(element, null));
+		this.rear = rear.getNext();
+		this.size++;
+	}
+	
+	public T dequeue(){
+		if(this.size == 0){
+			System.out.println("Queue is empty");
+			return null;
+		}
+		T temp = (T) this.front.getNext().getElement();
+		Node<T> nTemp = this.front.getNext();
+		this.front.setNext(nTemp.getNext());
+		if (this.rear == nTemp){
+			this.rear = this.front;
+		}
+		this.size--;
+		return temp;
+	}
+	
+	public Object first(){
+		if(this.size == 0){
+			System.out.println("Queue is empty");
+			return null;
+		}
+		return this.front.getNext().getElement();
+	}
+	
+	public int size(){
+		return this.size;
+	}
+	
+	public void clear(){
+		this.front = new Node<>();
+		this.rear = this.front;
+		this.size = 0;
+	}
+	
+	public String toString(){
+		String result = "*LQueue*\n";
+		Node<T> tFront = this.front;
+		int tSize = this.size;
+		while(tSize != 0){
+			result+= tFront.getNext().getElement() + " ";
+			tFront = tFront.getNext();
+			tSize--;
+		}
+		return result;
+	}
+
+    /**
+     * @param args the command line arguments
+     */
+        
+    public static void main(String[] args) {
+        
+    }
+        // TODO code application logic here
+    }
+    
+

e47b9cd27b93248b731172224c8cfa3964a8ed7e
fiorosello09 <fiorosello09@hotmail.com>
2019-10-14 13:44:31 -0600

    Merge origin/master into Version1

---
+++ src/Controller/ControllerPrincipal.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author fioro
+ */
+public class ControllerPrincipal 
+{
+
+    /**
+     *
+     * @param nombre
+     * @param fecha
+     * @param pasaporte
+     * @param nacionalidad
+     * @param origen
+     * @param destino
+     * @param planLealtad
+     */
+    public void CheckIn(String nombre, String fecha, String pasaporte, String nacionalidad,String origen, String destino, String planLealtad)
+    {
+        
+    }
+    
+}
